4bd5822cbcb3dd39d3e80dcc6d9eb86e7e27d91e
Player01osu <player01osu@gmail.com>
2022-05-28 02:03:22 -0400

    Add

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@ pub struct ThreadPool;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
     }
 }
