7978962ce13fe2c751d66e4d201ef609d254f11a
player01osu <player01osu@gmail.com>
2022-06-01 11:05:33 -0400

    Normalize routing
    - Paths get normalize
    - Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 use actix_files::NamedFile;
-use actix_web::{HttpResponse, HttpRequest, Result, guard, get};
+use actix_web::{get, http::Method, middleware, web, App, HttpServer, HttpResponse};
 use actix_web::http::StatusCode;
 use std::path::PathBuf;
 
-async fn index(_req: HttpRequest) -> Result<NamedFile, actix_web::Error> {
+async fn index() -> Result<NamedFile, actix_web::Error> {
     let path: PathBuf = "./files/index.html".parse().unwrap();
 
     Ok(NamedFile::open(path)?)
 }
 
-async fn test(_req: HttpRequest) -> Result<NamedFile, actix_web::Error> {
+async fn test() -> Result<NamedFile, actix_web::Error> {
     let path: PathBuf = "./files/test.html".parse().unwrap();
 
     Ok(NamedFile::open(path)?)
 }
 
-async fn not_found() -> Result<HttpResponse> {
-    Ok(HttpResponse::build(StatusCode::OK)
-        .content_type("text/html; charset=utf-8")
-        .body("<h1>Error 404</h1>"))
+async fn not_found() -> Result<NamedFile, actix_web::Error> {
+    let path: PathBuf = "./files/404.html".parse().unwrap();
+
+    Ok(NamedFile::open(path)?)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_web::{web, App, HttpServer};
-
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::NormalizePath::trim())
             .service(web::resource("/").to(index))
-            .service(web::resource("/test").to(test))
+            .service(web::resource("/testI").to(test))
             .default_service(web::route().to(not_found))
         })
+        .bind("127.0.0.1:8080")?
         .run()
         .await
 }
