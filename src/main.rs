a173b00307882ea0c21abee7fc854f1391beace2
player01osu <player01osu@gmail.com>
2022-06-01 19:37:00 -0400

    Cleaup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use actix_files::NamedFile;
+use actix_web::HttpRequest;
 use actix_web::{get, http::Method, middleware, web, App, HttpServer, HttpResponse};
 use actix_web::http::StatusCode;
 use std::path::PathBuf;
 
-async fn index() -> Result<NamedFile, actix_web::Error> {
-    let path: PathBuf = "./files/index.html".parse().unwrap();
-
-    Ok(NamedFile::open(path)?)
-}
-
-async fn test() -> Result<NamedFile, actix_web::Error> {
-    let path: PathBuf = "./files/test.html".parse().unwrap();
+async fn index(req: HttpRequest) -> Result<NamedFile, actix_web::Error> {
+    let path: PathBuf = match &req.path()[..] {
+        "/" => "./files/index.html".parse().unwrap(),
+        "/index.html" => "./files/index.html".parse().unwrap(),
+        "/index" => "./files/index.html".parse().unwrap(),
+        "/testI.html" => "./files/test.html".parse().unwrap(),
+        "/testI" => "./files/test.html".parse().unwrap(),
+        _ => "./files/404.html".parse().unwrap(),
+    };
 
     Ok(NamedFile::open(path)?)
 }
@@ -27,7 +29,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(middleware::NormalizePath::trim())
             .service(web::resource("/").to(index))
-            .service(web::resource("/testI").to(test))
+            .service(web::resource("/{index}").to(index))
             .default_service(web::route().to(not_found))
         })
         .bind("127.0.0.1:8080")?
