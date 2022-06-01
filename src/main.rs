aeeed9374d8797dd8135d314a4bff82a3d6d921a
player01osu <player01osu@gmail.com>
2022-06-01 00:51:50 -0400

    Route invalid to 404 page + Cleanup Index
    - Invalid urls get routed to 404 page
    - Remove unnecessary code from index()

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use actix_files::NamedFile;
-use actix_web::{HttpRequest, Result};
+use actix_web::{HttpResponse, HttpRequest, Result, guard, get};
+use actix_web::http::StatusCode;
 use std::path::PathBuf;
 
 async fn index(_req: HttpRequest) -> Result<NamedFile, actix_web::Error> {
-    let path: PathBuf = if _req.path().eq("/") {
-        "./files/index.html".parse().unwrap()
-    } else if _req.path().eq("/about") {
-        "./files/404.html".parse().unwrap()
-    } else {
-        "./files/404.html".parse().unwrap()
-    };
+    let path: PathBuf = "./files/index.html".parse().unwrap();
 
     Ok(NamedFile::open(path)?)
+}
+
+async fn not_found() -> Result<HttpResponse> {
+    Ok(HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body("<h1>Error 404</h1>"))
 }
 
 #[actix_web::main]
@@ -21,8 +22,15 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
         .route("/", web::get().to(index))
-        .route("/about", web::get().to(index))
 
+        .service(
+            web::resource("/user/{name}")
+                .name("user_detail")
+                .guard(guard::Header("content-type", "application/json"))
+                .route(web::get().to(HttpResponse::Ok))
+                .route(web::put().to(HttpResponse::Ok)),
+        )
+        .default_service(web::route().to(not_found))
         })
         .bind(("127.0.0.1", 8080))?
         .run()
