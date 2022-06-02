ccc0dbf9e8eb9320094528d399ad6efe7604a05d
player01osu <player01osu@gmail.com>
2022-06-02 08:08:48 -0400

    Set logging environment variables

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ async fn not_found() -> Result<NamedFile, actix_web::Error> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
     HttpServer::new(|| App::new()
         .service(fs::Files::new("/", "./files/").index_file("index.html"))
         .default_service(web::route().to(not_found)))
