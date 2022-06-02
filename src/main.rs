8bfd28f7300a055fd36467fbc487c12f5aeaa656
player01osu <player01osu@gmail.com>
2022-06-01 23:05:49 -0400

    Web server can properly serve static pages
    + Static pages are now statically served through directory
    + Static pages can properly serve css style sheets
    
    TODO:
    
    - Properly implement 404 error page

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,9 @@ use actix_web::HttpRequest;
 use actix_web::{get, http::Method, middleware, web, App, HttpServer, HttpResponse};
 use actix_web::http::StatusCode;
 use std::path::PathBuf;
+use actix_files as fs;
 
-async fn index(req: HttpRequest) -> Result<NamedFile, actix_web::Error> {
-    let path: PathBuf = match &req.path()[..] {
-        "/" => "./files/index.html".parse().unwrap(),
-        "/index.html" => "./files/index.html".parse().unwrap(),
-        "/index" => "./files/index.html".parse().unwrap(),
-        "/testI.html" => "./files/test.html".parse().unwrap(),
-        "/testI" => "./files/test.html".parse().unwrap(),
-        _ => "./files/404.html".parse().unwrap(),
-    };
-
-    Ok(NamedFile::open(path)?)
-}
-
+// TODO: Properly implement 404 Error.
 async fn not_found() -> Result<NamedFile, actix_web::Error> {
     let path: PathBuf = "./files/404.html".parse().unwrap();
 
@@ -25,14 +14,10 @@ async fn not_found() -> Result<NamedFile, actix_web::Error> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .wrap(middleware::NormalizePath::trim())
-            .service(web::resource("/").to(index))
-            .service(web::resource("/{index}").to(index))
-            .default_service(web::route().to(not_found))
-        })
-        .bind("127.0.0.1:8080")?
+    HttpServer::new(|| App::new()
+        .service(fs::Files::new("/", "./files/").index_file("index.html"))
+        .default_service(web::route().to(not_found)))
+        .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
