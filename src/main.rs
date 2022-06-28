d2529262a1008becaa20f03b87c412077ff6cccb
player01osu <player01osu@gmail.com>
2022-06-28 19:14:37 -0400

    Advanced routing and static file serving
    remove unnessessary

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 mod api;
+mod routing;
 
-use std::ops::Index;
+use std::path::PathBuf;
 use actix_web::middleware::Logger;
-use actix_web::{Responder, get, http::Method, middleware, web, App, HttpServer, HttpResponse, HttpRequest};
+use actix_web::web::Bytes;
+use actix_web::{
+    Responder,
+    get,
+    http::Method,
+    middleware,
+    web,
+    App,
+    HttpServer,
+    HttpResponse,
+    HttpRequest,
+    Result,
+};
 use actix_web::http::StatusCode;
 use actix_files::NamedFile;
 use actix_web::{
@@ -12,14 +25,18 @@ use actix_web::{
     web::Json,
     web::Path,
 };
-use actix_rt;
-use actix::prelude::*;
+use actix_rt; use actix::prelude::*;
 use actix_files as fs;
+use routing::routes;
 use serde::{Deserialize, Serialize};
-
 
 use mongodb::{Client, options::ClientOptions};
 use mongodb::bson::{doc, Document};
+use once_cell::sync::*;
+use dashmap::DashMap;
+use compact_str::*;
+use ahash::RandomState;
+
 
 use api::task::{
     not_found,
@@ -27,6 +44,8 @@ use api::task::{
     MyActor,
     Ping
 };
+
+use routes::*;
 
 #[derive(Serialize, Deserialize)]
 struct Identity {
@@ -39,28 +58,47 @@ async fn index(index: Path<Identity>) -> impl Responder {
     format!("Hello {}! id:{}", index.name, index.id)
 }
 
-//#[get("/")]
-//async fn static_handler() -> impl Responder {
-//    fs::Files::new("/", "./files/").index_file("index.html")
-//}
+fn main() -> anyhow::Result<()> {
+    init()
+}
+
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+pub async fn run() -> std::io::Result<()> {
     // Logging
     std::env::set_var("RUST_LOG", "debug");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    //init().ok();
+    //let (static_served_from, static_mount_path): (String, PathBuf) = get_static().unwrap();
+
+    //let ff: PathBuf = "./static/assets/".into();
+    // Insert 404 Page Not Found
+    let not_found_page = StaticFile {
+        bytes: Bytes::from(include_bytes!("./static/404.html").to_vec()),
+        path: PathBuf::from("static/404.html"),
+    };
+    let route_handle = RouteHandle { response: not_found_page };
+    ROUTEMAP.insert("{{404}}".into(), route_handle);
+
 
     HttpServer::new(move|| {
         let logger = Logger::default();
-        App::new()
-        .wrap(logger)
-        //.service(static_handler)
-        .service(index)
-        .service(get_task)
-        .service(fs::Files::new("/", "./files/").index_file("index.html"))
-        .default_service(web::route().to(not_found))
+        let app_instance = App::new()
+            .wrap(logger)
+            //.service(static_handler)
+            .service(index)
+            .service(get_task)
+            .service({
+                //let static_serve = actix_files::Files::new(&static_served_from, &static_mount_path);
+                let static_serve = actix_files::Files::new("/assets", "static/assets");
+                static_serve
+                })
+            //.service(fs::Files::new("/", "./files/").index_file("index.html"))
+            .default_service(web::route().to(router));
+
+        app_instance
     })
     .bind(("127.0.0.1", 8080))?
     .run()
