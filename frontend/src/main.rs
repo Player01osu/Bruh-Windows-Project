bade84aa24e752e05817839975e7c1af6d21212c
player01osu <player01osu@gmail.com>
2022-07-12 02:44:33 -0400

    Basic expand on click

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,173 +1,128 @@
 use reqwasm::http::{Headers, Request};
 use serde::{Deserialize, Serialize};
+use yew::html::Scope;
 use yew::prelude::*;
 use yew::{html, Component, Context, Html};
 
+struct App {
+    images: Vec<Image>,
+}
+
+enum Msg {
+    ToggleExpando(usize),
+}
+
+#[derive(Clone, PartialEq)]
+enum ImageExpandState {
+    Unfocus,
+    Focus,
+}
+
 #[derive(Properties, Clone, PartialEq)]
 struct Image {
-    title: String,
-    author: String,
-    path: String,
-    width: String,
-    height: String,
+    pub state: ImageExpandState,
+    pub title: String,
+    pub author: String,
+    pub path: String,
+    pub time: usize,
+    pub width: String,
+    pub height: String,
 }
 
-#[derive(Properties, PartialEq)]
-struct ImageListProps {
-    images: Vec<Image>,
-    on_click: Callback<Image>,
-}
-
-#[derive(Properties, PartialEq, Clone)]
-struct ImageDetailsProps {
-    image: Image,
-}
-
-#[function_component(ImageDetails)]
-fn image_details(ImageDetailsProps { image }: &ImageDetailsProps) -> Html {
-    html! {
-        <div>
-            <h3>{ image.title.clone() }</h3>
-            <img
-                src={format!("{}", image.path)}
-                alt={format!("{}", image.title)}
-                width="300"
-                height="300"
-            />
-        </div>
-    }
-}
-
-#[function_component(ImageList)]
-fn image_list(ImageListProps { images, on_click }: &ImageListProps) -> Html {
-    let on_click = on_click.clone();
-    images
-        .iter()
-        .map(|image| {
-            let on_image_select = {
-                image.width = "300".to_string();
-                image.height = "300".to_string();
-                let on_click = on_click.clone();
-                let image = image.clone();
-                Callback::from(move |_| {
-                    //image.width = "300".to_string();
-                    //image.height = "300".to_string();
-                    on_click.emit(image.clone())
-                })
-            };
-
-            html! {
-                <img
-                    onclick={on_image_select}
-                    src={format!("{}", image.path)}
-                    alt={format!("{}", image.title)}
-                    width={format!("{}", image.width)}
-                    height={format!("{}", image.height)}
-                />
+impl Image {
+    pub fn toggle_expand(&mut self) {
+        match &self.state {
+            ImageExpandState::Focus => {
+                self.height = "300".to_string();
+                self.width = "300".to_string();
+                self.state = ImageExpandState::Unfocus
             }
-        })
-        .collect()
-}
-
-impl Component for Image {
-    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::Expand => {
-                self.width = "400";
-                self.height = "400";
-                true
+            ImageExpandState::Unfocus => {
+                self.height = "100".to_string();
+                self.width = "100".to_string();
+                self.state = ImageExpandState::Focus
             }
         }
     }
-    fn view(&self, ctx: &Context<Self>) -> Html {
+}
 
+impl App {
+    pub fn view_images(&self, image_id: usize, image: &Image, link: &Scope<Self>) -> Html {
+
+        html! {
+            <div>
+                <img src={format!("{}", image.path)}
+                    width={format!("{}", image.width)}
+                    height={format!("{}", image.height)}
+                    onclick={link.callback(move |_| Msg::ToggleExpando(image_id))}/>
+            </div>
+        }
+    }
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
         let images = vec![
             Image {
+                state: ImageExpandState::Unfocus,
                 title: "Yuri mmm I love ".to_string(),
                 author: "ur mom ".to_string(),
-                path: "assets/posts/test.jpg ".to_string(),
+                time: 21,
+                path: "test.jpg ".to_string(),
                 width: "100".to_string(),
                 height: "100".to_string(),
             },
             Image {
+                state: ImageExpandState::Unfocus,
                 title: "Ay".to_string(),
                 author: "bro".to_string(),
+                time: 2001,
                 path: "assets/img/blah.jpg ".to_string(),
                 width: "100".to_string(),
                 height: "100".to_string(),
             },
         ];
 
-        let selected_image = use_state(|| None);
+        Self { images }
+    }
 
-        let on_image_select = {
-            let selected_image = selected_image.clone();
-            Callback::from(move |image: Image| selected_image.set(Some(image)))
-        };
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::ToggleExpando(image_id) => {
+                let image = self.images.get_mut(image_id).unwrap();
 
-        let details = selected_image.as_ref().map(|image| {
+                image.toggle_expand();
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let posts = self.images.chunks(21).enumerate().map(|(id, images)| {
+            let image_list = images
+                .iter()
+                .enumerate()
+                .map(|(id, image)| self.view_images(id, image, ctx.link()));
             html! {
-                <ImageDetails image={image.clone()} />
+                <div>
+                    {for image_list}
+                </div>
             }
         });
 
         html! {
             <>
                 <div>
-                    <h1>{ "ell" }</h1>
-
-                    <ImageList images={images} on_click={on_image_select.clone()}/>
+                    <h1>{ "Click on image to expand!" }</h1>
+                    { for posts }
                 </div>
-                { for details }
             </>
         }
     }
 }
-
-//#[function_component(App)]
-//fn app() -> Html {
-//    let images = vec![
-//        Image {
-//            title: "Yuri mmm I love ".to_string(),
-//            author: "ur mom ".to_string(),
-//            path: "assets/posts/test.jpg ".to_string(),
-//            width: "100".to_string(),
-//            height: "100".to_string(),
-//        },
-//        Image {
-//            title: "Ay".to_string(),
-//            author: "bro".to_string(),
-//            path: "assets/img/blah.jpg ".to_string(),
-//            width: "100".to_string(),
-//            height: "100".to_string(),
-//        },
-//    ];
-//
-//    let selected_image = use_state(|| None);
-//
-//    let on_image_select = {
-//        let selected_image = selected_image.clone();
-//        Callback::from(move |image: Image| selected_image.set(Some(image)))
-//    };
-//
-//    let details = selected_image.as_ref().map(|image| {
-//        html! {
-//            <ImageDetails image={image.clone()} />
-//        }
-//    });
-//
-//    html! {
-//        <>
-//            <div>
-//                <h1>{ "ell" }</h1>
-//
-//                <ImageList images={images} on_click={on_image_select.clone()}/>
-//            </div>
-//            { for details }
-//        </>
-//    }
-//}
-
 fn main() {
     yew::start_app::<App>();
 }
