74aa9ee57791fbea687ab5cc5ac3ab3dfc69462e
player01osu <player01osu@gmail.com>
2022-07-15 18:49:58 -0400

    Shitty script to send json posts to backend

---
+++ post-to-db.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+
+prompt () {
+# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+printf "\033[1;34mAuthor: \nOP: \nTags Wrapped in quotes (ex: \"nuts\", \"fart\", ...): \nFile name: \n%s\033[1;34m: \033[0m" "$1"
+read -r REPLY
+}
+prompt "Title"
+TITLE="$REPLY"
+
+prompt () {
+# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+OP:
+Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...):
+File name:
+%s\033[1;34m: \033[0m" "$TITLE" "$1"
+read -r REPLY
+}
+prompt "Author"
+AUTHOR="$REPLY"
+
+prompt () {
+# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+Author: \033[0m%s\033[1;34m
+Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...):
+File name:
+%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$1"
+read -r REPLY
+}
+prompt "OP"
+OP="$REPLY"
+
+TIME=$(date +%s)
+
+prompt () {
+# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+Author: \033[0m%s\033[1;34m
+OP: \033[0m%s\033[1;34m
+File name:
+%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$OP" "$1"
+read -r REPLY
+}
+prompt "Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...)"
+TAGS="$REPLY"
+
+prompt () {
+# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+Author: \033[0m%s\033[1;34m
+OP: \033[0m%s\033[1;34m
+TAGS: \033[0m%s\033[1;34m
+%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$OP" "$TAGS" "$1"
+read -r REPLY
+}
+prompt "File name"
+FILENAME="$REPLY"
+
+FORMATED="{\"title\":\"$TITLE\",\"author\":\"$AUTHOR\",\"op\":\"$OP\",\"tags\":[$TAGS],\"time\":$TIME,\"file_name\":\"$FILENAME\"}"
+
+echo $TITLE $AUTHOR $OP $TIME $TAGS $FILENAME
+echo $FORMATED
+
+set -v
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data "$FORMATED" \
+  http://localhost:7878/api/post_image
