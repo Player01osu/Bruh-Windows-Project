9c6dae44f4653521dfad61a33d36711070e92cf8
player01osu <player01osu@gmail.com>
2022-07-15 12:25:15 -0400

    Impl default trait for YuriPosts, PostStats and Comment

--- common/src/mongodb/structs.rs
+++ common/src/mongodb/structs.rs
@@ -7,7 +7,22 @@ pub struct YuriPosts {
     pub op: String,
     pub tags: Vec<String>,
     pub path: String,
+    pub comments: Vec<Comment>,
+    pub post_stats: PostStats,
     pub time: u64,
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+pub struct Comment {
+    pub op: String,
+    pub body: String,
+    pub time: u64,
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
+pub struct PostStats {
+    pub likes: u64,
+    pub views: u64,
 }
 
 #[derive(Clone, PartialEq, Deserialize, Debug)]
@@ -32,9 +47,44 @@ pub struct ImageRequest {
     pub op: String,
     pub time: usize,
     pub tags: Vec<String>,
+    pub post_stats: PostStats,
     pub path: String,
+    pub comments: Vec<Comment>,
 }
 
+impl Default for YuriPosts {
+    fn default() -> Self {
+        Self {
+            title: "No_Title".to_string(),
+            author: "No_Author".to_string(),
+            op: "No_OP".to_string(),
+            tags: [String::new()].to_vec(),
+            path: "EMPTY".to_string(),
+            comments: [Comment::default()].to_vec(),
+            post_stats: PostStats::default(),
+            time: 0,
+        }
+    }
+}
+
+impl Default for PostStats {
+    fn default() -> Self {
+        Self {
+            likes: 0,
+            views: 0,
+        }
+    }
+}
+
+impl Default for Comment {
+    fn default() -> Self {
+        Self {
+            op: "None".to_string(),
+            body: String::new(),
+            time: 0
+        }
+    }
+}
 
 pub enum ImageMessage {
     ToggleExpando(usize),
