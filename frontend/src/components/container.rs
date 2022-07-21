13efffc12011df05cf1037216d9933463b6a6aba
player01osu <player01osu@gmail.com>
2022-07-21 14:35:30 -0400

    Collapsible sidebar
    TODO:
    	- Need to make it look pretty

--- frontend/src/components/container.rs
+++ frontend/src/components/container.rs
@@ -1,4 +1,5 @@
 use yew::{html, Component, Context, Html};
+use super::sidebar::Sidebar;
 
 pub struct Container;
 
@@ -13,50 +14,7 @@ impl Component for Container {
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             <div class={"container"}>
-                <div class="navall">
-                    <div class="nav">
-                            <form action="" class="search-bar">
-                                <input type="text" class="search" placeholder="search tag or somth" name="q"/>
-                            </form>
-                            <div class="nav-img">
-                                <div>
-                                    <img class="imge" src="assets/img/blah.jpg" alt="nav-img"/>
-                                </div>
-                            </div>
-                        <center>
-                            <div class="links">
-                                <div class="indiv">
-                                    <div>
-                                        <a href="layout2.html"
-                                            class="link"
-                                            style="text-decoration: none;">{"LAYOUT2"}</a>
-                                    </div>
-                                </div>
-                                <div class="indiv">
-                                    <div>
-                                        <a href="tags.html"
-                                            class="link"
-                                            style="text-decoration: none;">{"TAGS"}</a>
-                                    </div>
-                                </div>
-                                <div class="indiv">
-                                    <div>
-                                        <a href="layout2.html"
-                                            class="link"
-                                            style="text-decoration: none;">{"ABOUT"}</a>
-                                    </div>
-                                </div>
-                                <div class="indiv">
-                                    <div>
-                                        <a href="about.html"
-                                            class="link"
-                                            style="text-decoration: none;">{"SAMPLE"}</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </center>
-                    </div>
-                </div>
+                <Sidebar/>
             </div>
         }
     }
--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,2 +1,3 @@
 pub mod posts;
 pub mod container;
+pub mod sidebar;
