3140313ce2e785af39a7df5c2359f7ca0e93649b
player01osu <player01osu@gmail.com>
2022-07-23 12:29:02 -0400

    Routing and staticish main page

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,8 @@
 mod components;
 mod gallery;
 
-use std::fmt::Display;
-
-use components::posts::Posts;
+use components::header::Header;
+use components::container::Container;
 use gallery::{Gallery, GalleryViews, GalleryTop};
 use yew::{html, Component, Context, Html};
 use yew_router::{BrowserRouter, Switch, Routable};
@@ -34,7 +33,7 @@ pub enum GalleryRoute {
 
 pub fn switch(route: &Route) -> Html {
     match route {
-        Route::Home => todo!(),
+        Route::Home => html!{ <StaticPages/> },
         Route::GalleryRouter => html!{
             <Switch<GalleryRoute> render={Switch::render(gallery_switch)} />
         },
@@ -49,6 +48,46 @@ pub fn gallery_switch(route: &GalleryRoute) -> Html {
         GalleryRoute::New => html!{ <Gallery sort="new"/> },
         GalleryRoute::Top => html!{ <GalleryTop sort="top"/> },
         GalleryRoute::Views => html!{ <GalleryViews sort="views"/> },
+    }
+}
+
+pub enum StaticMsg {
+    LoadPage(Html),
+}
+
+struct StaticPages {
+    page: Html,
+}
+
+impl Component for StaticPages {
+    type Message = StaticMsg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            page: html!{},
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            StaticMsg::LoadPage(page_html) => {
+                self.page = page_html;
+                true
+            }
+        }
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html!{
+            <>
+                <body style="background-color: black;">
+                    <Header/>
+                    <Container/>
+                    <h2 style="position: relative; margin-top: 20px; margin-left: 80px;">{ "Welcome! to the Wholesome Yuri website" }</h2>
+                </body>
+            </>
+        }
     }
 }
 
