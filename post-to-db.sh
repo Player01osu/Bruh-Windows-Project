b2b5585d60787c7bc892146aa67b7db464c04c6a
player01osu <player01osu@gmail.com>
2022-07-27 16:17:59 -0400

    Image posting script option to move image to posts directory

--- post-to-db.sh
+++ post-to-db.sh
@@ -2,55 +2,55 @@
 
 
 prompt () {
-# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
-printf "\033[1;34mAuthor: \nOP: \nTags Wrapped in quotes (ex: \"nuts\", \"fart\", ...): \nFile name: \n%s\033[1;34m: \033[0m" "$1"
-read -r TITLE
+	# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+	printf "\033[1;34mAuthor: \nOP: \nTags Wrapped in quotes (ex: \"nuts\", \"fart\", ...): \nFile name: \n%s\033[1;34m: \033[0m" "$1"
+	read -r TITLE
 }
 prompt "Title"
 
 prompt () {
-# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
-printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
-OP:
-Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...):
-File name:
-%s\033[1;34m: \033[0m" "$TITLE" "$1"
-read -r AUTHOR
+	# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+	printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+	OP:
+	Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...):
+	File name:
+	%s\033[1;34m: \033[0m" "$TITLE" "$1"
+	read -r AUTHOR
 }
 prompt "Author"
 
 prompt () {
-# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
-printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
-Author: \033[0m%s\033[1;34m
-Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...):
-File name:
-%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$1"
-read -r OP
+	# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+	printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+	Author: \033[0m%s\033[1;34m
+	Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...):
+	File name:
+	%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$1"
+	read -r OP
 }
 prompt "OP"
 
 TIME=$(date +%s)
 
 prompt () {
-# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
-printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
-Author: \033[0m%s\033[1;34m
-OP: \033[0m%s\033[1;34m
-File name:
-%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$OP" "$1"
-read -r TAGS
+	# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+	printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+	Author: \033[0m%s\033[1;34m
+	OP: \033[0m%s\033[1;34m
+	File name:
+	%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$OP" "$1"
+	read -r TAGS
 }
 prompt "Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...)"
 
 prompt () {
-# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
-printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
-Author: \033[0m%s\033[1;34m
-OP: \033[0m%s\033[1;34m
-Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...): \033[0m%s\033[1;34m
-%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$OP" "$TAGS" "$1"
-read -r FILENAME
+	# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+	printf "\n\n\033[1;34mTitle: \033[0m%s\033[1;34m
+	Author: \033[0m%s\033[1;34m
+	OP: \033[0m%s\033[1;34m
+	Tags Wrapped in quotes (ex: \"nuts\", \"fart\", ...): \033[0m%s\033[1;34m
+	%s\033[1;34m: \033[0m" "$TITLE" "$AUTHOR" "$OP" "$TAGS" "$1"
+	read -r FILENAME
 }
 prompt "File name"
 
@@ -64,3 +64,26 @@ curl --header "Content-Type: application/json" \
   --request POST \
   --data "$FORMATED" \
   http://localhost:7878/api/post_image & echo $AUTHOR-$TIME-$FILENAME | xclip -sel clip
+
+prompt () {
+	# prompts the user with message in $1-2 ($1 in blue, $2 in magenta) and saves the input to the variables in $REPLY and $REPLY2
+	printf "\n\n\033[1;34mMove image into posts directory? [Y/n]: \033[0m"
+	read -r MOVEPOST
+}
+
+case $MOVEPOST in
+	"n")
+	"N") return;;
+	"y")
+	"Y")
+	"")
+		printf "\n\n\033[1;34mInput path the image is located (ie: ~/Pictures/stuffs/test-image.jpg OR ../other/directory): \033[0m"
+		read -r IMAGEPATH
+		;;
+esac
+
+if [ -z $IMAGEPATH || -f $IMAGEPATH]; then
+	echo post-to-db.sh: ERROR: Image path did not exist
+else
+	cp $IMAGEPATH ./frontend/assets/posts/$AUTHOR-$TIME-$FILENAME
+fi
