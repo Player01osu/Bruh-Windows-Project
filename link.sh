65a5c677ea3ed8d492cfa56257e095ee79f18615
player01osu <player01osu@gmail.com>
2022-07-27 16:24:14 -0400

    Create directory if empty, and add release and debug flags

--- link.sh
+++ link.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
 
-rm -rf ./target/debug/static
-rm -rf ./target/debug/static.json
+if [ ! -d ./frontend/assets/posts ]; then
+	echo posts directory does not exist\; Creating ./frontend/assets/posts/...
+	mkdir -p ./frontend/assets/posts/
+fi
+if [ ! -d ./frontend/dist ]; then
+	echo dist directory does not exist\; Creating ./frontend/dist...
+	mkdir -p ./frontend/dist
+fi
 
-ln -s $(pwd)/backend/src/static ./target/debug/static
-ln -s $(pwd)/backend/static.json ./target/debug/
+case $1 in
+	"--release")
+		rm -rf ./target/**/static
+		rm -rf ./target/**/static.json
+		rm -rf ./target/**/dist
+		rm -rf ./target/**/assets
+
+		ln -s $(pwd)/backend/src/static ./target/release/static
+		ln -s $(pwd)/backend/static.json ./target/release/
+		ln -s $(pwd)/frontend/assets ./target/release/
+		ln -s $(pwd)/frontend/dist ./backend/
+		;;
+	"--debug")
+	"")
+		rm -rf ./target/**/static
+		rm -rf ./target/**/static.json
+		rm -rf ./target/**/dist
+		rm -rf ./target/**/assets
+
+		ln -s $(pwd)/backend/src/static ./target/debug/static
+		ln -s $(pwd)/backend/static.json ./target/debug/
+		ln -s $(pwd)/frontend/assets ./target/debug/
+		ln -s $(pwd)/frontend/dist ./backend/
+		;;
+esac
