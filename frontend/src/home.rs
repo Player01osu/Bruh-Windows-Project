eea7eb38bc1c36738c1eb77b665b2c25eb704253
player01osu <player01osu@gmail.com>
2022-08-03 22:12:31 -0400

    Split home into seperate file

---
+++ frontend/src/home.rs
@@ -0,0 +1,41 @@
+use super::components::{container::Container, template::Template};
+use yew::{html, Component, Context, Html};
+
+pub enum HomeMsg {
+    LoadPage(Html),
+}
+
+pub struct Home {
+    page: Html,
+}
+
+impl Component for Home {
+    type Message = HomeMsg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { page: html! {} }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            HomeMsg::LoadPage(page_html) => {
+                self.page = page_html;
+                true
+            }
+        }
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                <Template>
+                    <Container/>
+                    <h2 style="position: relative; margin-top: 100px; margin-left: 200px;">{
+                        "Welcome! to the Wholesome Yuri website"
+                    }</h2>
+                </Template>
+            </>
+        }
+    }
+}
