3582843d9483f407d1e350dfcb13485e38f33560
player01osu <player01osu@gmail.com>
2022-08-03 22:12:40 -0400

    Split routing into seperate file

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,112 +1,14 @@
 mod about;
 mod components;
 mod gallery;
+mod home;
 mod not_found;
+mod routes;
 mod tags;
 
-use about::About;
-use components::container::Container;
-use components::header::Header;
-use components::template::Template;
-use gallery::{Gallery, Sort};
-use not_found::NotFound;
-use tags::Tags;
+use routes::{switch, Route};
 use yew::{html, Component, Context, Html};
-use yew_router::prelude::*;
-use yew_router::{BrowserRouter, Routable, Switch};
-
-#[derive(Clone, Routable, PartialEq)]
-pub enum Route {
-    #[at("/")]
-    Home,
-    #[at("/gallery/:sort")]
-    GalleryRouter,
-    #[at("/gallery")]
-    Gallery,
-    #[at("/about")]
-    About,
-    #[at("/tags")]
-    Tags,
-    #[not_found]
-    #[at("/404")]
-    NotFound,
-}
-
-#[derive(Clone, Routable, PartialEq)]
-pub enum GalleryRoute {
-    #[at("/gallery/new")]
-    New,
-    #[at("/gallery/top")]
-    Top,
-    #[at("/gallery/views")]
-    Views,
-    #[not_found]
-    #[at("/gallery/404")]
-    NotFound,
-}
-
-pub fn switch(route: &Route) -> Html {
-    match route {
-        Route::Home => html! { <StaticPages/> },
-        Route::GalleryRouter => html! {
-            <Switch<GalleryRoute> render={Switch::render(gallery_switch)} />
-        },
-        Route::Gallery => html! { <Gallery sort={Sort::New}/> },
-        Route::About => html! { <About/> },
-        Route::Tags => html! { <Tags/> },
-        Route::NotFound => html! { <NotFound/> },
-    }
-}
-
-pub fn gallery_switch(route: &GalleryRoute) -> Html {
-    match route {
-        GalleryRoute::New => html! { <Gallery sort={Sort::New}/> },
-        GalleryRoute::Top => html! { <Gallery sort={Sort::Top}/> },
-        GalleryRoute::Views => html! { <Gallery sort={Sort::Views}/> },
-        GalleryRoute::NotFound => html! {
-            <Redirect<Route> to={Route::NotFound}/>
-        },
-    }
-}
-
-pub enum StaticMsg {
-    LoadPage(Html),
-}
-
-struct StaticPages {
-    page: Html,
-}
-
-impl Component for StaticPages {
-    type Message = StaticMsg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { page: html! {} }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            StaticMsg::LoadPage(page_html) => {
-                self.page = page_html;
-                true
-            }
-        }
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <>
-                <Template>
-                    <Container/>
-                    <h2 style="position: relative; margin-top: 100px; margin-left: 200px;">{
-                        "Welcome! to the Wholesome Yuri website"
-                    }</h2>
-                </Template>
-            </>
-        }
-    }
-}
+use yew_router::{BrowserRouter, Switch};
 
 struct App;
 
@@ -126,6 +28,7 @@ impl Component for App {
         }
     }
 }
+
 fn main() {
     yew::start_app::<App>();
 }
