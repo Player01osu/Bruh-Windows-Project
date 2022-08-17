1b24f11bc81e1dab8705862368edf4cabc9ddf0c
player01osu <player01osu@gmail.com>
2022-08-17 17:51:34 -0400

    Like post persist with local storage key value

--- backend/src/api/post/like_post.rs
+++ backend/src/api/post/like_post.rs
@@ -1,20 +1,71 @@
-use common::mongodb::structs::YuriPosts;
-use crate::database::mongo::{CollectionList, MongodbDatabase};
+use crate::{
+    api::user::Users,
+    database::mongo::{CollectionList, MongodbDatabase},
+};
+use common::mongodb::structs::{ImageLiked, UsersDb, YuriPosts};
+use serde::Deserialize;
 
-use actix_web::{put, web::{Data, Path}, HttpResponse};
+use actix_web::{
+    put,
+    web::{Data, Path},
+    HttpResponse,
+};
 use bson::oid::ObjectId;
-use mongodb::bson::doc;
+use mongodb::{
+    bson::doc,
+    options::{FindOptions, UpdateOptions},
+};
 
-#[put("/like-post/{oid}")]
-pub async fn like_post(
-    path: Path<String>,
-    database: Data<MongodbDatabase>,
-) -> HttpResponse {
-    // Parse oid into ObjectId object type
-    let oid = ObjectId::parse_str(&path.as_str()).unwrap();
-    let filter = doc! ("_id": oid);
-    let add_like = doc! { "$inc": { "stats.likes": 1 } };
+#[derive(Deserialize)]
+pub struct PathInfo {
+    oid: String,
+    private: String,
+}
+
+#[put("/like-post/{oid}/{private}")]
+pub async fn like_post(path: Path<PathInfo>, database: Data<MongodbDatabase>) -> HttpResponse {
+    let users_collection = database.collection::<UsersDb>(CollectionList::Users);
     let posts_collection = database.collection::<YuriPosts>(CollectionList::Posts);
+
+    // Parse oid into ObjectId object type
+    let oid = ObjectId::parse_str(&path.oid.as_str()).unwrap();
+    let filter = doc!("_id": oid);
+    let add_like = doc! { "$inc": { "stats.likes": 1 } };
+
+    let public = Users::hash_priv(&path.private);
+
+    let update_query = doc!("public": &public, "image_states._id": &oid);
+    let update_user = doc! (
+        "$set": {
+            "image_states.$.like_state": "Liked"
+        }
+    );
+
+    match users_collection
+        .update_one(update_query, update_user, None)
+        .await
+    {
+        Ok(r) => {
+            if r.modified_count == 0 {
+                // Push if post_id doesn't exist
+                let update_query = doc!(
+                    "public": &public,
+                );
+                let update_user = doc! (
+                    "$push": { "image_states": {
+                        "_id": &oid,
+                        "like_state": "Liked"
+                        }
+                    }
+                );
+                users_collection
+                    .update_one(update_query, update_user, None)
+                    .await
+                    .expect("Fail to write to database");
+            }
+        }
+        Err(e) => todo!("{e}"),
+    };
 
     posts_collection
         .update_one(filter, add_like, None)
@@ -24,16 +75,50 @@ pub async fn like_post(
     HttpResponse::Ok().body("HTTP/1.1 201 Updated")
 }
 
-#[put("/unlike-post/{oid}")]
-pub async fn unlike_post(
-    path: Path<String>,
-    database: Data<MongodbDatabase>,
-) -> HttpResponse {
-    // Parse oid into ObjectId object type
-    let oid = ObjectId::parse_str(&path.as_str()).unwrap();
-    let filter = doc! ("_id": oid);
-    let remove_like = doc! { "$inc": { "stats.likes": -1 } };
+#[put("/unlike-post/{oid}/{private}")]
+pub async fn unlike_post(path: Path<PathInfo>, database: Data<MongodbDatabase>) -> HttpResponse {
     let posts_collection = database.collection::<YuriPosts>(CollectionList::Posts);
+    let users_collection = database.collection::<UsersDb>(CollectionList::Users);
+
+    // Parse oid into ObjectId object type
+    let oid = ObjectId::parse_str(&path.oid.as_str()).unwrap();
+    let filter = doc!("_id": oid);
+    let remove_like = doc! { "$inc": { "stats.likes": -1 } };
+
+    let public = Users::hash_priv(&path.private);
+
+    let update_query = doc!("public": &public, "image_states._id": &oid);
+    let update_user = doc! (
+        "$set": {
+            "image_states.$.like_state": "Unliked"
+        }
+    );
+
+    match users_collection
+        .update_one(update_query, update_user, None)
+        .await
+    {
+        Ok(r) => {
+            if r.modified_count == 0 {
+                // Push if post_id doesn't exist
+                let update_query = doc!(
+                    "public": &public,
+                );
+                let update_user = doc! (
+                    "$push": { "image_states": {
+                        "_id": &oid,
+                        "like_state": "Unliked"
+                        }
+                    }
+                );
+                users_collection
+                    .update_one(update_query, update_user, None)
+                    .await
+                    .expect("Fail to write to database");
+            }
+        }
+        Err(e) => todo!("{e}"),
+    };
 
     posts_collection
         .update_one(filter, remove_like, None)
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,11 +6,14 @@ mod session;
 use crate::session::Session;
 use components::{container::Container, template::Template};
 use gloo_utils::window;
+use pages::gallery::GalleryMsg;
 use routes::{switch, Route};
-use yew::{html, Component, Context, Html};
+use yew::{html, Component, Context, Html, ContextProvider};
 use yew_router::{BrowserRouter, Switch};
 
-pub struct App;
+pub struct App {
+    session: Session,
+}
 
 pub struct AppMsg(Session);
 
@@ -20,7 +23,9 @@ impl Component for App {
 
     fn create(ctx: &Context<Self>) -> Self {
         Session::init(&ctx.link());
-        Self
+        Self {
+            session: Default::default()
+        }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -31,22 +36,30 @@ impl Component for App {
 
         local_storage
             .set_item("public", &msg.0.user_pub)
-            .expect("Failed to set private id");
-        if !msg.0.user_priv.is_empty() {
+            .expect("Failed to set public id");
+        if let Some(private) = &msg.0.user_priv {
             local_storage
-                .set_item("private", &msg.0.user_priv.to_string())
-                .expect("Failed to set public id");
+                .set_item("private", private)
+                .expect("Failed to set private id")
+        };
+
+        self.session.update_state(msg.0);
+        if let Some(callback) = &self.session.gallery_callback {
+            callback.emit(GalleryMsg::Reload);
         }
         true
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
+        let session = self.session.clone();
         html! {
             <>
                 <BrowserRouter>
                     <Container/>
                     <Template>
+                        <ContextProvider<Session> context={session}>
                             <Switch<Route> render={Switch::render(switch)} />
+                        </ContextProvider<Session>>
                     </Template>
                 </BrowserRouter>
             </>
